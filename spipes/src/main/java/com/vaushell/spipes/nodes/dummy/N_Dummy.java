dec99cdf6136b0fd1b2f5ba796ec224634d3ec9a
Fabien Vauchelles <fabien@vauchelles.com>
2013-12-15 23:34:46 +0100

    add dummy node

---
+++ spipes/src/main/java/com/vaushell/spipes/nodes/dummy/N_Dummy.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2013 Fabien Vauchelles (fabien_AT_vauchelles_DOT_com).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3, 29 June 2007, of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+package com.vaushell.spipes.nodes.dummy;
+
+import com.vaushell.spipes.nodes.A_Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Fabien Vauchelles (fabien_AT_vauchelles_DOT_com)
+ */
+public class N_Dummy
+    extends A_Node
+{
+    // PUBLIC
+    public N_Dummy()
+    {
+        super();
+    }
+
+    // PROTECTED
+    @Override
+    protected void prepareImpl()
+        throws Exception
+    {
+        // Nothing
+    }
+
+    @Override
+    protected void loop()
+        throws Exception
+    {
+        // Receive
+        final Object message = getLastMessageOrWait();
+
+        if ( LOGGER.isTraceEnabled() )
+        {
+            LOGGER.trace( "[" + getNodeID() + "] receive and send message : " + message );
+        }
+
+        sendMessage( message );
+    }
+
+    @Override
+    protected void terminateImpl()
+        throws Exception
+    {
+        // Nothing
+    }
+
+    // PRIVATE
+    private static final Logger LOGGER = LoggerFactory.getLogger( N_Dummy.class );
+}
