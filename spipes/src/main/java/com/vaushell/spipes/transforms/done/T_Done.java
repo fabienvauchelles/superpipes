7b9b2137a83baaf3d06b23f42c4f376908f687ef
Fabien Vauchelles <fabien@vauchelles.com>
2013-12-19 17:22:21 +0100

    misc

--- spipes/src/main/java/com/vaushell/spipes/transforms/done/T_Done.java
+++ spipes/src/main/java/com/vaushell/spipes/transforms/done/T_Done.java
@@ -30,6 +30,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.HashSet;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
