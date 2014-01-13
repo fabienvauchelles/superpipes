f3307a5863e6cc48072b2da34a80b6a1a9bce40c
Fabien Vauchelles <fabien@vauchelles.com>
2014-01-13 23:48:01 +0100

    add a T_CheckURI transform to verify if an URI exists. Use a default HTTP timeout of 5000ms (parameter "timeout")

---
+++ spipes/src/main/java/com/vaushell/spipes/transforms/uri/T_CheckURI.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2013 Fabien Vauchelles (fabien_AT_vauchelles_DOT_com).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3, 29 June 2007, of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+package com.vaushell.spipes.transforms.uri;
+
+import com.vaushell.spipes.dispatch.Message;
+import com.vaushell.spipes.transforms.A_Transform;
+import java.io.IOException;
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Check if the URI exists.
+ *
+ * @author Fabien Vauchelles (fabien_AT_vauchelles_DOT_com)
+ */
+public class T_CheckURI
+    extends A_Transform
+{
+    // PUBLIC
+    public T_CheckURI()
+    {
+        super();
+
+        this.client = null;
+    }
+
+    @Override
+    public void prepare()
+        throws Exception
+    {
+        this.client = HttpClientBuilder
+            .create()
+            .setDefaultCookieStore( new BasicCookieStore() )
+            .setUserAgent( "Mozilla/5.0 (Windows NT 5.1; rv:15.0) Gecko/20100101 Firefox/15.0.1" )
+            .build();
+    }
+
+    @Override
+    public Message transform( final Message message )
+        throws Exception
+    {
+        // Receive
+        if ( LOGGER.isTraceEnabled() )
+        {
+            LOGGER.trace( "[" + getNode().getNodeID() + "/" + getClass().getSimpleName() + "] transform message : " + message );
+        }
+
+        if ( !message.contains( Message.KeyIndex.URI ) )
+        {
+            return null;
+        }
+
+        final URI uri = (URI) message.getProperty( Message.KeyIndex.URI );
+
+        try
+        {
+            if ( isURIvalid( uri ) )
+            {
+                return message;
+            }
+            else
+            {
+                if ( LOGGER.isTraceEnabled() )
+                {
+                    LOGGER.trace( "[" + getNode().getNodeID() + "/" + getClass().getSimpleName() + "] Invalid URI : " + uri.
+                        toString() );
+                }
+
+                return null;
+            }
+        }
+        catch( final IOException ex )
+        {
+            LOGGER.warn( "Error while checking URI" ,
+                         ex );
+
+            return null;
+        }
+    }
+
+    @Override
+    public void terminate()
+        throws IOException
+    {
+        if ( client != null )
+        {
+            client.close();
+        }
+    }
+
+    // PRIVATE
+    private static final Logger LOGGER = LoggerFactory.getLogger( T_CheckURI.class );
+    private CloseableHttpClient client;
+
+    private boolean isURIvalid( final URI uri )
+        throws IOException
+    {
+        HttpEntity responseEntity = null;
+        try
+        {
+            // Exec request
+            final HttpGet get = new HttpGet( uri );
+
+            int timeout = 5000;
+
+            final String timeoutStr = getConfig( "timeout" );
+            if ( timeoutStr != null )
+            {
+                try
+                {
+                    timeout = Integer.parseInt( timeoutStr );
+                }
+                catch( final NumberFormatException ex )
+                {
+                    // Ignore
+                }
+            }
+
+            get.setConfig(
+                RequestConfig.custom()
+                .setConnectTimeout( timeout )
+                .setConnectionRequestTimeout( timeout )
+                .setSocketTimeout( timeout )
+                .build()
+            );
+
+            try( final CloseableHttpResponse response = client.execute( get ) )
+            {
+                final StatusLine sl = response.getStatusLine();
+                responseEntity = response.getEntity();
+
+                return sl.getStatusCode() >= 200 && sl.getStatusCode() < 300;
+            }
+        }
+        finally
+        {
+            if ( responseEntity != null )
+            {
+                EntityUtils.consume( responseEntity );
+            }
+        }
+    }
+}
